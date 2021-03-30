0b10817fc61e84844fd69f09706879dc551b22d0
Pritesh Shinde <priteshs@prdxn.com>
2021-03-30 15:34:33 +0530

    Linting implemented using airbnb

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-/**** Core modules */
+/** ** Core modules */
 const fs = require('fs');
 const { join } = require('path');
-/**** Core modules */
+/** ** Core modules */
 
-/**** 3rd party modules */
+/** ** 3rd party modules */
 const { urlencoded, json: bodyParserJson } = require('body-parser');
 const express = require('express');
 const rateLimit = require('express-rate-limit');
@@ -11,22 +11,22 @@ const morgan = require('morgan');
 const swaggerJsDoc = require('swagger-jsdoc');
 const swaggerUi = require('swagger-ui-express');
 require('dotenv').config();
-/**** 3rd party modules */
+/** ** 3rd party modules */
 
-/**** Local modules */
+/** ** Local modules */
 const { swaggerDefinition, swaggerOptions } = require('./src/config/serverConfig');
 const { responseMsg } = require('./src/helpers/utils');
-/**** Local modules */
+/** ** Local modules */
 
 const app = express();
 
-/**** Morgan Logger for logging each request into custom log files */
-let logDir = join(__dirname, 'src/logs', 'access.log');
+/** ** Morgan Logger for logging each request into custom log files */
+const logDir = join(__dirname, 'src/logs', 'access.log');
 if (!fs.existsSync(logDir)) {
   if (!fs.existsSync(join(__dirname, 'src/logs'))) {
     fs.mkdirSync(join(__dirname, 'src/logs'));
   }
-  fs.writeFileSync(logDir, '', err => {
+  fs.writeFileSync(logDir, '', (err) => {
     if (err) console.error(err);
 
     console.info('The log file is created succesfully!');
@@ -35,7 +35,7 @@ if (!fs.existsSync(logDir)) {
 const accessLogStream = fs.createWriteStream(logDir, { flags: 'a' });
 
 app.use(morgan('combined', { stream: accessLogStream }));
-/**** Morgan Logger for logging each request into custom log files */
+/** ** Morgan Logger for logging each request into custom log files */
 
 /**
  * @name helmet
@@ -47,7 +47,8 @@ app.use(require('helmet')());
 
 /**
  * @name compression
- * @description This middleware will compress the assets which are to be sent in the response of server.
+ * @description This middleware will compress the assets
+ * which are to be sent in the response of server.
  * Compress the assets to be sent in response
  * For further information: https://www.npmjs.com/package/compression
  */
@@ -65,23 +66,23 @@ app.use(require('express-status-monitor')());
 app.set('port', process.env.HTTP_PORT || 8000);
 app.set('app URL', process.env.APP_URL || 'localhost:8000');
 app.set('title', process.env.APP_NAME);
-app.set('query parser', `extended`);
+app.set('query parser', 'extended');
 
 /** Importing database connection when server starts */
 require('./src/config/dbConfig');
 
-/**** Setting up the CORS for app */
+/** ** Setting up the CORS for app */
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', process.env.CLIENT_URL || '*');
   res.header('Access-Control-Allow-Headers', 'Origin, Authorization, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS, HEAD');
-  if ('OPTIONS' === req.method) {
+  if (req.method === 'OPTIONS') {
     res.sendStatus(200);
   } else {
     next();
   }
 });
-/**** Setting up the CORS for app */
+/** ** Setting up the CORS for app */
 
 /** Form encryption application/x-www-form-urlencoded */
 app.use(urlencoded({ limit: '50mb', extended: false }));
@@ -91,7 +92,8 @@ app.use(bodyParserJson({ limit: '50mb' }));
 
 /**
  * @name xss-clean
- * @description This middleware will sanitize user input coming from POST body, GET queries, and url params.
+ * @description This middleware will sanitize user input
+ * coming from POST body, GET queries, and url params.
  * This will sanitize any data in req.body, req.query, and req.params.
  * For further information: https://www.npmjs.com/package/xss-clean
  */
@@ -99,14 +101,14 @@ app.use(require('xss-clean')());
 
 /** Express Rate Limit for DOS attack prevention */
 app.use(rateLimit({
-  windowMs: 1 * 60 * 1000,    // 1 minute
-  max: 100                    // limit each IP to 100 requests per windowMs
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 100, // limit each IP to 100 requests per windowMs
 }));
 
 /** serve static files */
 app.use(express.static(join(__dirname, 'src/public')));
 
-app.enable('etag');           // use strong etags
+app.enable('etag'); // use strong etags
 app.set('etag', 'strong');
 
 /**
@@ -119,7 +121,7 @@ app.use(
   swaggerUi.setup(swaggerJsDoc({
     swaggerDefinition,
     apis: ['./src/routes/*.js'],
-  }), swaggerOptions)
+  }), swaggerOptions),
 );
 
 /** Configuring Routes */
