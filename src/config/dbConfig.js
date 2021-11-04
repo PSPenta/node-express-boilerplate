7cbc749e1aeea9c4cec917941672e5108bac17e2
Pritesh Shinde <pritesh.s@purplle.com>
2021-11-04 11:18:54 +0530

    Code optimised

--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -1,5 +1,4 @@
 /* eslint-disable import/no-dynamic-require */
-/* eslint-disable max-len */
 /* eslint-disable import/no-unresolved */
 /* eslint-disable global-require */
 const { readdirSync } = require('fs');
@@ -102,15 +101,29 @@ if (database.toLowerCase() === 'mongodb') {
   /** ** Establishing Relationships */
   /** Sequelize One-To-One relationship */
   // this.model('User').hasOne(this.model('Profile'));
-  // this.model('Profile').belongsTo(this.model('User'), { constraints: true, onDelete: 'CASCADE' });
+  // this.model('Profile').belongsTo(this.model('User'), {
+  //   constraints: true,
+  //   onDelete: 'CASCADE'
+  // });
 
   /** Sequelize One-To-Many relationship */
   // this.model('User').hasMany(this.model('Product'));
-  // this.model('Product').belongsTo(this.model('User'), { constraints: true, onDelete: 'CASCADE' });
+  // this.model('Product').belongsTo(this.model('User'), {
+  //   constraints: true,
+  //   onDelete: 'CASCADE'
+  // });
 
   /** Sequelize Many-To-Many relationship */
-  // this.model('User').belongsToMany(this.model('Product'), { through: this.model('UserProducts'), constraints: true, onDelete: 'CASCADE' });
-  // this.model('Product').belongsToMany(this.model('User'), { through: this.model('UserProducts'), constraints: true, onDelete: 'CASCADE' });
+  // this.model('User').belongsToMany(this.model('Product'), {
+  //   through: this.model('UserProducts'),
+  //   constraints: true,
+  //   onDelete: 'CASCADE'
+  // });
+  // this.model('Product').belongsToMany(this.model('User'), {
+  //   through: this.model('UserProducts'),
+  //   constraints: true,
+  //   onDelete: 'CASCADE'
+  // });
   /** ** Establishing Relationships */
 
   sequelize.sync()
