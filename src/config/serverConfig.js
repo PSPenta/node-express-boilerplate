a5f0da4eadf50f17a6e475767d2f0d92b893db7f
Pritesh Shinde <pritesh.shinde@rupyy.com>
2023-08-20 17:50:10 +0530

    MongoDB replicaSet setup added

--- src/config/serverConfig.js
+++ src/config/serverConfig.js
@@ -7,9 +7,9 @@ const noSqlDbConfig = {
 };
 
 const noSqlReplicaSetDbConfig = {
-  url1: process.env.DB_URL || 'mongodb://localhost:27017/',
-  url2: process.env.DB_URL || 'mongodb://localhost:27017/',
-  url3: process.env.DB_URL || 'mongodb://localhost:27017/',
+  url1: process.env.DB_URL1 || 'mongodb://localhost:27017/',
+  url2: process.env.DB_URL2 || 'mongodb://localhost:27017/',
+  url3: process.env.DB_URL3 || 'mongodb://localhost:27017/',
   name: process.env.DB_NAME || 'myDB'
 };
 
